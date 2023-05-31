c32da2ba98c30f08343eace1b695e822b0b1cce0
ErjanUralov <uraloverjan777@gmail.com>
2023-05-31 08:33:26 +0500

    Bu yerda JS 6-dars kodlari bor
    JS 6-dars kodlari yuklandi

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/* localStarge - shaxsiy, mahhalling ombor */
+
+/* localStorage.setItem('ism' , 'Behruz', );
+// localStorage.setItem('familiya' ,'muhammadov' );
+// localStorage.setItem('yoshi','13 yashda');
+
+// localStorage['ism']="xondamir";
+// localStorage['familiya'] = "Qo`ldoshev";
+
+/*localStorage.removeItem('ism');
+localStorage.removeItem('familiya');
+localStorage.removeItem('yoshi');
+
+// localStorage.clear()
+
+// let uningIsmi = localStorage.getItem('ism');
+// let uningfamiliyasi = localStorage.getItem('familiya');
+// let uningYoshi = localStorage.getItem('yoshi')
+
+// console.log(uningIsmi,uningfamiliyasi,uningYoshi)*/
+
+/*let sorov = new XMLHttpRequest();
+sorov.open('get','https://getty.uz/serverdan/malumot/olish');
+sorov.send();
+
+sorov.onload = function (){
+    console.log(sorov.responseText);
+}*/
+
+/* JSon in JS */
+//let jsFile = {ism: "Behruz",yoshi: 13};              /*JS*/
+
+//let jsonFile = '{"ismi":"Bilolxoja","yoshi":14}'    /*JSON*/
+//console.log(JSON.stringify(jsFile));
+//console.log(JSON.parse(jsonFile));
+
+let sorov = new XMLHttpRequest();
+sorov.open('get','jsonFile.json');
+sorov.send();
+sorov.onload = function (){
+    let byData = JSON.parse(sorov.responseText)
+    console.log(byData);
+}
+
+
+
+
+
+
